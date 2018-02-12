215e59883760dd8b6661ff9b241838ac90470499
AntoineYnov <antoine.genouinduhamel@ynov.com>
2018-02-12 17:20:02 +0100

    pas eu le temps d'avancer dessus, j'ai passé 1 minute

---
+++ GarageVoiture/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 12 févr. 2018
+ *      Author: Antoi
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+	string choix;
+	cout << "Choississez une voiture" << endl;
+	cin >> choix;
+	cout << choix;
+
+	return 0;
+}
+
+
